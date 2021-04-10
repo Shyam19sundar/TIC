1c13129b64b6470f1cdae54281763ef2f71e13ed
Ramprakash <ramprakash1024@outlook.com>
2021-04-10 20:13:54 +0530

    TIC

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.use(
         credentials: true,
     })
 );
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Headers", "*")
+    next()
+})
+
 // Configure the bodyParser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
